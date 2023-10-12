c3f93c5af777220fed646c2fc91cb84bffb53923
LMikadaL <panupong.mikada@gmail.com>
2023-10-13 02:51:39 +0700

    udpate download wordpress

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -2,8 +2,9 @@
 
 /etc/init.d/php7.4-fpm start
 
-wget https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz -C /var/www/html
+wget -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
+mkdir -p /var/www/html/wordpress
+tar -xzvf /tmp/wordpress.tar.gz -C /var/www/html
 chown -R www-data.www-data /var/www/html/wordpress
 chmod -R 755 /var/www/html/wordpress
 
